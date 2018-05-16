5ac62cd9834b561efcdaf5d36728de6baaed1cca
yanxrxr <ansonyxr@gmail.com>
2018-05-16 23:11:17 +0800

    Enemies disappear after being shot

---
+++ src/Tank.java
@@ -0,0 +1,64 @@
+
+public class Tank {
+	int x;
+	int y;
+	int direction = 2;
+	int speed = 1;
+	boolean isAlive = true;
+	
+	public Tank(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.direction = 0;
+		this.speed = 1;
+	}
+
+	public int getDirection() {
+		return direction;
+	}
+
+	public void setDirection(int direction) {
+		this.direction = direction;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void moveUp() {
+		y -= speed;
+	}
+	
+	public void moveRight() {
+		x += speed;
+	}
+	
+	public void moveDown() {
+		y += speed;
+	}
+	
+	public void moveLeft() {
+		x -= speed;
+	}
+	
+}
